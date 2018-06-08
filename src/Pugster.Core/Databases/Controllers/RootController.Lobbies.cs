82fc04f52618fc4a8376d6b97e311d363c13801f
Aux <justin@akitaux.com>
2018-06-07 19:54:38 -0500

    Clean up RootController

---
+++ src/Pugster.Core/Databases/Controllers/RootController.Lobbies.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pugster
+{
+    public partial class RootController
+    {
+        public Task<bool> LobbyExistsAsync(ulong id)
+            => _db.Lobbies.AnyAsync(x => x.Id == id);
+        public Task<bool> LobbyExistsAsync(string name)
+            => _db.Lobbies.AnyAsync(x => x.Name.ToLower() == name.ToLower());
+        public Task<bool> LobbyHasPlayerAsync(ulong lobbyId, ulong profileId)
+            => _db.Players.AnyAsync(x => x.LobbyId == lobbyId && x.ProfileId == profileId);
+
+        public Task<int> GetTotalOpenLobbiesAsync()
+            => _db.Lobbies.CountAsync(x => x.IsOpen);
+
+        public Task<Lobby> GetLobbyAsync(ulong id)
+            => _db.Lobbies.SingleOrDefaultAsync(x => x.Id == id);
+        public Task<Lobby> GetLobbyAsync(string name)
+            => _db.Lobbies.SingleOrDefaultAsync(x => x.Name.ToLower() == name.ToLower());
+        public Task<List<Lobby>> GetLobbiesAsync()
+            => _db.Lobbies.ToListAsync();
+        public Task<List<Lobby>> GetOpenLobbiesAsync(int skip = 0, int take = 10)
+            => _db.Lobbies.Where(x => x.IsOpen).Skip(skip).Take(take).ToListAsync();
+        public async Task<List<Lobby>> FindLobbiesAsync(string name, int distance = 3, int take = 3)
+        {
+            var lobby = await GetLobbyAsync(name);
+            if (lobby != null) return new List<Lobby> { lobby };
+
+            var allLobbies = await GetLobbiesAsync();
+            var lobbyResult = allLobbies.Where(x => MathHelper.GetStringDistance(x.Name.ToLower(), name.ToLower()) <= distance).Take(take);
+            return lobbyResult.ToList();
+        }
+
+        public async Task<Lobby> CreateLobbyAsync(Lobby lobby)
+        {
+            lobby.CreatedAt = DateTime.UtcNow;
+            await _db.Lobbies.AddAsync(lobby);
+            await _db.SaveChangesAsync();
+            return lobby;
+        }
+        public async Task<Lobby> ModifyLobbyAsync(Lobby lobby)
+        {
+            await _db.SaveChangesAsync();
+            return lobby;
+        }
+    }
+}
---
+++ src/Pugster.Core/Databases/Controllers/RootController.Players.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pugster
+{
+    // Players
+    public partial class RootController
+    {
+        public Task<bool> PlayerExistsAsync(ulong id)
+            => _db.Lobbies.AnyAsync(x => x.Id == id);
+
+        public Task<int> GetLobbyTotalPlayersAsync(ulong lobbyId)
+            => _db.Players.CountAsync(x => x.LobbyId == lobbyId);
+
+        public Task<Player> GetPlayerAsync(ulong id)
+            => _db.Players.SingleOrDefaultAsync(x => x.Id == id);
+        public Task<List<Player>> GetPlayersAsync(ulong lobbyId)
+            => _db.Players.Where(x => x.LobbyId == lobbyId).ToListAsync();
+        public Task<List<Player>> GetLobbyPlayersAsync(ulong lobbyId)
+            => _db.Players.Where(x => x.LobbyId == lobbyId).ToListAsync();
+        public Task<Player> GetPlayerFromLobbyAsync(ulong lobbyId, ulong profileId)
+            => _db.Players.FirstOrDefaultAsync(x => x.LobbyId == lobbyId && x.ProfileId == profileId);
+
+        public async Task<Player> CreatePlayerAsync(Player player)
+        {
+            player.JoinedAt = DateTime.UtcNow;
+            await _db.Players.AddAsync(player);
+            await _db.SaveChangesAsync();
+            return player;
+        }
+        public async Task DeletePlayerAsync(Player player)
+        {
+            _db.Remove(player);
+            await _db.SaveChangesAsync();
+        }
+        public async Task<Player> ModifyPlayerAsync(Player player)
+        {
+            await _db.SaveChangesAsync();
+            return player;
+        }
+    }
+}
